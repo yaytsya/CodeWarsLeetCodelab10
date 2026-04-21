f5c5e895de68ea243035581119c40c36cbf944a1
yaytsya <fales.alex.2008@gmail.com>
2026-04-21 21:22:34 +0300

    Create DeleteNodeinBST.py
    створив папку і зробив завдання

---
+++ DeleteNodeinBST/DeleteNodeinBST.py
@@ -0,0 +1,23 @@
+class Solution:
+    def deleteNode(self, root: Optional[TreeNode], key: int) -> Optional[TreeNode]:
+        if not root:
+            return None
+
+        if key < root.val:
+            root.left = self.deleteNode(root.left, key)
+        elif key > root.val:
+            root.right = self.deleteNode(root.right, key)
+        else:
+            if not root.left:
+                return root.right
+            if not root.right:
+                return root.left
+
+            successor = root.right
+            while successor.left:
+                successor = successor.left
+
+            root.val = successor.val
+            root.right = self.deleteNode(root.right, successor.val)
+
+        return root
